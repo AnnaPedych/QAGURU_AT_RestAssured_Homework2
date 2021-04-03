8e6abb652a93a5c20c4120ced0168ac616674130
AnnaPedych <apedych@gmail.com>
2021-04-03 17:29:39 +0300

    added remote driver and allure

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,10 +2,14 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+import static config.ConfigHelper.isVideoOn;
+import static helpers.AttachmentsHelper.*;
+import static helpers.AttachmentsHelper.attachVideo;
+import static helpers.DriverHelper.*;
 
 
 public class TestBase {
@@ -13,10 +17,23 @@ public class TestBase {
     static void setUp() {
         RestAssured.baseURI = "http://demowebshop.tricentis.com";
         Configuration.baseUrl = "http://demowebshop.tricentis.com";
+        configureDriver();
     }
 
     public String getInitialWishlistCount() {
         open("");
         return $("a[href='/wishlist'] .wishlist-qty").getText();
+    }
+
+    @AfterEach
+    public void addAttachments(){
+        String sessionId = getSessionId();
+
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachAsText("Browser console logs", getConsoleLogs());
+        if (isVideoOn()) attachVideo(sessionId);
+
+        closeWebDriver();
     }
 }
